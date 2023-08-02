fbd484201f9395bbefe352178fc54e7685fcb1fa
Axel G <115911859+AxelG@users.noreply.github.com>
2023-08-02 13:54:13 -0600

    Add files via upload

--- macos/install.py
+++ macos/install.py
@@ -211,39 +211,31 @@ make_window_active()
 
     try:
         readme_script = '''    
-CocoaTabs Clicker
-Python grid based clicker.
+## CocoaTabs Clicker v0.1.2 ##
 
+Python grid based clicker.
 This solution will allow you to automate the process of clicking on multiple dots spread across a grid.
 
-Why download it?
+## Why download it?
 This tool proves invaluable for repetitive tasks that involve clicking on specific grid intersections. Whether it's collecting data points from a structured spreadsheet, automating GUI testing, or assisting in pixel-perfect design verification, CocoaTabs Clicker significantly enhances productivity and accuracy by simplifying the process of precise mouse clicks.
 
-Warning: CocoaTabs may not work properly in desktop environments with customized themes and configurations.
+WARNING: CocoaTabs may not work properly in desktop environments with customized themes and configurations.
 
-Installation
-- Download the repo's ZIP file and extract the files from the ZIP weherever you may prefer.
+## Installation
+Once installed, create a shortcut for the new "CocoaTabs.bat" file, change its name to "CocoaTabs", set its icon to the "icon.ico"" file, and copy the shortcut to your Desktop directory or wherever you may prefer.
 
-How to use
-1. Open the terminal and navigate to the directory where you extracted the files.
+## How to use
+1. Execute the shortcut you created.
 
-2. Enter the following command to run the CocoaTabs Clicker:
+2. Two new windows will appear:
 
-Wndows:
+3. The CocoaTabs Grid window allows you to view, move and adjust the size of the grid across your screen.
 
-python cocoatabs.py
-macOS and Linux:
+4. The CocoaTabs Console window allows you to change the number of columns and rows of the grid. Once you entered an input, you can view the new grid by clicking on the Draw Grid button. To start the clicker, click on the Click Grid button.
 
-python3 cocoatabs.py
-3. Two new windows will appear:
+5. Don't move your mouse until the clicking process is finished!
 
-The CocoaTabs Grid window allows you to view, move and adjust the size of the grid across your screen.
-
-The CocoaTabs Console window allows you to change the number of columns and rows of the grid. Once you entered an input, you can view the new grid by clicking on the Draw Grid button. To start the clicker, click on the Click Grid button.
-
-Don't move your mouse until the clicking process is finished!
-
-4. Once the clicking process has finished, you can close the app by clicking on the Close App button.
+6.Once the clicking process has finished, you can close the app by clicking on the Close App button.
             '''
         readme_name = 'README.txt'
         filereadme_path = os.path.join(os.getcwd(), readme_name)
